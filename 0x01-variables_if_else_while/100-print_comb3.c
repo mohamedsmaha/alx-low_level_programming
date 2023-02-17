119611247ea5bc4bfb505915ac2e2d5d9d492c4c
mohamedsabrymohamedahmed@gmail.com <mohamedsabrymohamedahmed@gmail.com>
2023-02-17 08:51:41 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - print all alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48 ; i < 58 ; i++)
+{
+for (j = i + 1 ; j < 58 ; j++)
+{
+if (i != 48 || j != 49)
+{
+putchar(32);
+}
+putchar(i);
+putchar(j);
+if (j != 57 || i != 56)
+{
+putchar(44);
+}
+}
+}
+putchar('\n')
+return (0);
+}
+
