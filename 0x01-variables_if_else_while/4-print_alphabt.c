ecfd426ad8009405e89572c907f66c7788661eaf
mohamedsabrymohamedahmed@gmail.com <mohamedsabrymohamedahmed@gmail.com>
2023-02-17 08:11:44 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - print all alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123 ; i++)
+{
+if (i == 113 || i == 101)
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
