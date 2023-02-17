0a5104c9af5d648716c0346f4206c579c6e968c4
mohamedsabrymohamedahmed@gmail.com <mohamedsabrymohamedahmed@gmail.com>
2023-02-17 08:24:15 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - print all alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 48 ; i < 58 ; i++)
+{
+putchar(32);
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+}
+}
+putchar('\n');
+return (0);
+}
+
